72016b65e1f279031787fbe5d0f516c3aadbedf2
Sebastiaan Stok <s.stok@rollerscapes.net>
2023-11-27 11:26:54 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// See https://hupkit.github.io/hupkit/config.html
+
+return [
+    'branches' => [],
+    'host' => 'github.com',
+    'repository' => 'rollerworks/PdbSfBridge',
+    'schema_version' => 2,
+];
